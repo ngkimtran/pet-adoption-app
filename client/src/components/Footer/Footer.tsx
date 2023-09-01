a325507d3628868ad019d6c96316e9d926fa7da5
Kim Tran <ngockimtranle@gmail.com>
2023-09-01 13:43:56 +0300

    Fix styling

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,8 +1,5 @@
 const Footer = () => (
-  <div
-    className="text-center bg-dark p-2 text-white fs-6"
-    style={{ marginTop: "15rem" }}
-  >
+  <div className="text-center bg-dark p-2 text-white fs-6">
     &copy; Kim Tran - 2023
   </div>
 );
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -9,14 +9,16 @@ const Home = () => (
     <Helmet>
       <title>Home</title>
     </Helmet>
-    <Slider />
-    <hr
-      className="m-0 p-0 background-primary border-0 opacity-100"
-      style={{ height: "1.5rem" }}
-    />
-    <AnimalList />
-    <Suggestion />
-    <Actions />
+    <div style={{ marginBottom: "15rem" }}>
+      <Slider />
+      <hr
+        className="m-0 p-0 background-primary border-0 opacity-100"
+        style={{ height: "1.5rem" }}
+      />
+      <AnimalList />
+      <Suggestion />
+      <Actions />
+    </div>
   </>
 );
 

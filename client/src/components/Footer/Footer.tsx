49a7aba63e875ca34336c2897603c1af65caf205
Kim Tran <ngockimtranle@gmail.com>
2023-09-01 12:13:33 +0300

    Fix footer styling

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,5 +1,8 @@
 const Footer = () => (
-  <div className="text-center bg-dark p-2 text-white fs-6">
+  <div
+    className="text-center bg-dark p-2 text-white fs-6"
+    style={{ marginTop: "15rem" }}
+  >
     &copy; Kim Tran - 2023
   </div>
 );
